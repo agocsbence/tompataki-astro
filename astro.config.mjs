51b989782fe61f663cf5a432f4013fe6d46ff3f5
Bence <bence@harom.studio>
2025-11-22 23:14:48 +0100

    update mobile header

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,16 @@ import { defineConfig } from 'astro/config';
 import pagefind from "astro-pagefind";
 
 // https://astro.build/config
+// astro.config.mjs
 export default defineConfig({
-    integrations: [pagefind()],
+  vite: {
+    server: { fs: { allow: [process.cwd()] } },
+  },
+  integrations: [pagefind()],
+  devOptions: {
+    disableDevServer: false,
+    open: false,
+    port: 3000,
+    hmr: true
+  },
 });
