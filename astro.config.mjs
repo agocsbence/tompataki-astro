4cbe9d77d9637e5c1800a769c96cec2788c6efad
Bence <bence@harom.studio>
2026-02-18 00:28:44 +0100

    replace font files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,34 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  vite: {
+    server: { fs: { allow: [process.cwd()] } },
+  },
+
+  devOptions: {
+    disableDevServer: false,
+    open: false,
+    port: 3000,
+    hmr: true,
+  },
+
+  // Astro Assets: engedélyezzük a WP-s remote képeket optimalizálásra
   image: {
+    domains: ["cms.tompataki.com"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cms.tompataki.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tompataki.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.vimeocdn.com",
         pathname: "/**",
       },
     ],
